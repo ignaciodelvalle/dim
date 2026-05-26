872f71ab62f99442a2585b1b574cdcdb292e28c8
ignaciodelvalle2014@gmail.com <ignaciodelvalle2014@gmail.com>
2026-05-26 13:11:23 -0300

    feat(poncho): Sidebar + SidebarNav + MobileDrawer components

--- components/poncho/Layout/index.ts
+++ components/poncho/Layout/index.ts
@@ -1,3 +1,7 @@
 export { GobStripe } from "./GobStripe";
 export { AppHeader, type NavItem } from "./AppHeader";
 export { AppFooter } from "./AppFooter";
+export { Sidebar } from "./Sidebar";
+export { SidebarNav, type RoleAccent } from "./SidebarNav";
+export { MobileDrawer } from "./MobileDrawer";
+export { Topbar } from "./Topbar";
