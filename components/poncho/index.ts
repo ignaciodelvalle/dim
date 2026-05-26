6600fde2f01d22b44a87649c5d5d614e280107da
ignaciodelvalle2014@gmail.com <ignaciodelvalle2014@gmail.com>
2026-05-26 05:06:41 -0300

    feat(poncho): export Photo, Sheet, Crumbs from index

--- components/poncho/index.ts
+++ components/poncho/index.ts
@@ -31,3 +31,6 @@ export {
   type JurisdictionScope,
 } from "./JurisdictionSwitcher";
 export { PeriodPicker, type PeriodPickerProps, type PeriodPreset } from "./PeriodPicker";
+export { Photo, type PhotoProps, type PhotoStatus, type PhotoSize } from "./Photo";
+export { Sheet, type SheetProps, type SheetSide, type SheetSize } from "./Sheet";
+export { Crumbs, type CrumbsProps, type Crumb } from "./Crumbs";
