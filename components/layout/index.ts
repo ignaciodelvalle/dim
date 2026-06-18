dad065ca47d8e8fa506cd22262e612a6de2899f0
Ignacio del Valle <ignaciodelvalle2014@gmail.com>
2026-06-18 13:48:33 -0300

    feat(shell): migrate operator surfaces to AppShell variant=operator + ContextSwitcher (Item 7 phase B)

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -2,3 +2,6 @@ export { GobStripe } from "./GobStripe";
 export { AppHeader, type NavItem } from "./AppHeader";
 export { AppFooter } from "./AppFooter";
 export { AppShell } from "./AppShell";
+export { ContextSwitcher } from "./ContextSwitcher";
+export { AppShellDrawer } from "./AppShellDrawer";
+export type { DrawerNavSection } from "./AppShellDrawer";
